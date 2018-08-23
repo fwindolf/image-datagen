d125164dba431ea16f62cd5288b964ad35c6f299
Florian Windolf <windolf1@aalto.fi>
2018-08-23 10:01:36 +0300

    Added exports to generator to be able to import *.

---
+++ generator/__init__.py
@@ -0,0 +1,5 @@
+from .generator_image import AM2018ImageGenerator
+from .generator_text import AM2018TxtGenerator
+from .generator_mixed import AM2018MixedGenerator
+
+__all__ = ["AM2018ImageGenerator", "AM2018TxtGenerator", "AM2018MixedGenerator"]
